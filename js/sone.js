827ec399ccb9a3c256c8e431b9c14fd3bd9b8140
Zee Ma <ericma96@outlook.com>
2015-09-25 19:26:01 +0800

    Revert "delete"
    This reverts commit 8d49140e1c6e1bfba9b760e001f274ff09691feb.

---
+++ js/sone.js
@@ -0,0 +1,26 @@
+
+
+
+
+$(document).ready(function(){
+  $(".down-one").click(function(){
+  $(".member").animate({top:"-600px"});
+  });
+});
+$(document).ready(function(){
+  $(".up-two").click(function(){
+  $(".member").animate({top:"0px"});
+  });
+});
+
+$(document).ready(function(){
+  $(".down-two").click(function(){
+  $(".member").animate({top:"-1200px"});
+  });
+});
+
+$(document).ready(function(){
+  $(".up-three").click(function(){
+  $(".member").animate({top:"-600px"});
+  });
+});
