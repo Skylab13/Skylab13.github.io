1d81c99c6a4419bc64223c2eac1ca340f67454d5
Skylab13 <zhuyan@zhuyandeMacBook-Air.local>
2015-10-07 22:15:07 +0800

    最终修改

--- js/member.js
+++ js/member.js
@@ -1,12 +1,14 @@
+function move2(num){
+	switch(num){
+		case 1: document.getElementById("scroll2").style.WebkitTransform = "translateX(-100%)"; break;
+		case 2: document.getElementById("scroll2").style.WebkitTransform = "translateX(0%)"; break;
+		case 3: document.getElementById("scroll2").style.WebkitTransform = "translateX(-200%)"; break;
+		case 4: document.getElementById("scroll2").style.WebkitTransform = "translateX(-100%)"; break;
+	}
+}
 $(document).ready(function(){
 	$('.box').click(function(){
 		$('.box').fadeOut(2000);
 		$('.box').children('p').fadeOut(1000);
-	});
-	$('#radio-2').click(function(){
-		$('.fml').fadeOut(3000);
-	});
-	$('#radio-2').siblings('input').click(function(){
-		$('.fml').fadeIn(1000);
 	});
 });
