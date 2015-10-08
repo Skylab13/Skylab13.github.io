6194e942c602237ff355805781733d88c166b68c
Skylab13 <zhuyan@zhuyandeMacBook-Air.local>
2015-10-08 20:48:03 +0800

    成员简介bug修改

--- js/member.js
+++ js/member.js
@@ -1,9 +1,29 @@
 function move2(num){
 	switch(num){
-		case 1: document.getElementById("scroll2").style.WebkitTransform = "translateX(-100%)"; break;
-		case 2: document.getElementById("scroll2").style.WebkitTransform = "translateX(0%)"; break;
-		case 3: document.getElementById("scroll2").style.WebkitTransform = "translateX(-200%)"; break;
-		case 4: document.getElementById("scroll2").style.WebkitTransform = "translateX(-100%)"; break;
+		case 1: 
+			document.getElementById("scroll2").style.WebkitTransform = "translateX(-100%)";
+			document.getElementById("al2").style.display = "block"; 
+			document.getElementById("al1").style.display = "none" ;
+			document.getElementById("al3").style.display = "none" ;
+			break;
+		case 2: 
+			document.getElementById("scroll2").style.WebkitTransform = "translateX(0%)";
+			document.getElementById("al1").style.display = "block";
+			document.getElementById("al2").style.display = "none" ;
+			document.getElementById("al3").style.display = "none" ;
+			break;
+		case 3:
+			document.getElementById("scroll2").style.WebkitTransform = "translateX(-200%)";
+			document.getElementById("al3").style.display = "block";
+			document.getElementById("al1").style.display = "none" ;
+			document.getElementById("al2").style.display = "none" ;
+			break;
+		case 4: 
+			document.getElementById("scroll2").style.WebkitTransform = "translateX(-100%)";
+			document.getElementById("al2").style.display = "block";
+			document.getElementById("al1").style.display = "none" ;
+			document.getElementById("al3").style.display = "none" ;
+			break;
 	}
 }
 $(document).ready(function(){
